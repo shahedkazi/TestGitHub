77723949970b7a15fb7348aaee694f258ee06ad4
Shahed Kazi <shahed.kazi@lexisnexis.com.au>
2013-01-22 16:10:55 +1100

    Added Person class
    Added a new class

---
+++ GitTestApp/GitTestApp/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitTestApp
+{
+	class Person
+	{
+		public string FirstName, LastName;
+		public Person( string first, string last)
+		{
+			this.FirstName = first;
+			this.LastName = last;
+		}
+	}
+}
