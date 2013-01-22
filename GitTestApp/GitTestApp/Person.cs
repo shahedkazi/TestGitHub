2ca1c65e14b01a1889251380ac5305c89d9a9f92
Shahed Kazi <shahed.kazi@lexisnexis.com.au>
2013-01-23 10:05:47 +1100

    Commit to Second Branch
    Person class updated with DateOfBirth field

--- GitTestApp/GitTestApp/Person.cs
+++ GitTestApp/GitTestApp/Person.cs
@@ -8,15 +8,17 @@ namespace GitTestApp
 	class Person
 	{
 		public string FirstName, LastName;
-		public Person( string first, string last)
+		public DateTime DateOfBirth;
+		public Person( string first, string last, DateTime dob)
 		{
 			this.FirstName = first;
 			this.LastName = last;
+			this.DateOfBirth = dob;
 		}
 
 		public override string  ToString()
 		{
-			return this.FirstName + " " + this.LastName;
+			return this.FirstName + " " + this.LastName + " was born on " + this.DateOfBirth.ToShortDateString();
 		}
 	}
 }
