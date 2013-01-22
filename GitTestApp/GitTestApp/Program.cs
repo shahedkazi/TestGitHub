224b56615afad052f56200f4457fc1df2765a41e
Shahed Kazi <shahed.kazi@lexisnexis.com.au>
2013-01-22 14:40:38 +1100

    first commit

---
+++ GitTestApp/GitTestApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitTestApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Program p = new Program();
+			Console.WriteLine("Program starting");
+			Console.WriteLine("Enter i: ");
+			int i = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("Enter j: ");
+			int j = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("i + j = " + (i+j).ToString());
+			Console.ReadKey();
+		}
+
+
+		public int Add(int i, int j)
+		{
+			return i + j;
+		}
+	}
+}
